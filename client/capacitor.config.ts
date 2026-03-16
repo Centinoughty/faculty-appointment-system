6703d453783214821b66b9ddc2b333a4857dde60
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-03-17 05:26:38 +0530

    fixed security bugs

--- client/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nitc.fams',
-  appName: 'FAMS',
-  webDir: 'out'
-};
-
-export default config;
