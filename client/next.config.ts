9008a010077fb1d630833b4233d750bc666c4cb4
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2026-03-24 18:25:29 +0530

    fixed next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["192.168.42.37", "192.168.42.44"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "picsum.photos",
-      },{
+      },
+      {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
@@ -19,6 +21,17 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: "http://192.168.42.44:8000/api/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          { key: "Cache-Control", value: "no-cache" },
+          { key: "Content-Type", value: "application/javascript" },
+        ],
       },
     ];
   },
